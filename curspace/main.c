46d9e0e92052546bc1c32f6e689d7410ee968051
Michael Bayne <mdb@samskivert.com>
2011-09-22 08:48:27 -0700

    Initial Commit

---
+++ curspace/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  curspace
+//
+//  Created by Michael Bayne on 9/22/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
